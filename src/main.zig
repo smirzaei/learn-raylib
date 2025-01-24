50e65dba1b55bd450a7dea9c2ca699ebd127a5c3
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2025-01-24 17:27:55 -0500

    feat: create a window

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,35 @@
 const std = @import("std");
+const rl = @import("raylib");
+
+var counter = std.atomic.Value(usize).init(0);
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const width = 800;
+    const height = 600;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    rl.initWindow(width, height, "Hello, world!");
+    defer rl.closeWindow();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    rl.setTargetFPS(120);
 
-    try bw.flush(); // don't forget to flush!
+    _ = try std.Thread.spawn(.{}, handleCounter, .{});
+
+    while (!rl.windowShouldClose()) {
+        _ = counter.fetchAdd(1, .seq_cst);
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(rl.Color.fromInt(0x6495ed));
+        rl.drawFPS(0, 0);
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn handleCounter() void {
+    while (true) {
+        const now = std.time.timestamp();
+        const val = counter.load(.acquire);
+        std.debug.print("time: {d} - count: {d}\n", .{ now, val });
+        counter.store(0, .seq_cst);
+        std.time.sleep(std.time.ns_per_s);
+    }
 }
